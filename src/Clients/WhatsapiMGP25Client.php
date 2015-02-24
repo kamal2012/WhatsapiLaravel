775c64f445fcc3291511ce2c984cd7cf0dc5b944
Benjamín Martínez Mateos <bmxamin@gmail.com>
2015-02-24 11:51:29 -0600

    Commit inicial

---
+++ src/Clients/WhatsapiMGP25Client.php
@@ -0,0 +1,53 @@
+<?php 
+namespace Xaamin\WhatsapiLaravel\Clients;
+
+use Config;
+use WhatsProt;
+use Xaamin\WhatsapiLaravel\Repository\SMSMessageInterface;
+
+class WhatsapiMGP25Client implements SMSMessageInterface{
+
+    /**
+     * @var string $password
+     */
+    protected $password;
+
+    /**
+     * @var WhatsProt
+     */
+    protected $whatsProt;
+
+    /**
+     * @param WhatsProt $whatsProt
+     */
+    public function __construct(WhatsProt $whatsProt)
+    {
+        $this->whatsProt = $whatsProt;
+        $account   = Config::get("whatsapi-laravel::useAccount");
+        $this->password = Config::get("whatsapi-laravel::accounts.$account.password");
+    }
+
+    public function sendMessage($to, $message)
+    {
+        $this->connectAndLogin();
+        $this->whatsProt->sendMessageComposing($to);
+        $this->whatsProt->sendMessage($to, $message);
+        $this->logoutAndDisconnect();
+    }
+
+    public function checkForNewMessages()
+    {
+        // TODO: Implement checkForNewMessages() method.
+    }
+
+    protected function connectAndLogin()
+    {
+        $this->whatsProt->connect();
+        $this->whatsProt->loginWithPassword($this->password);
+    }
+
+    protected function logoutAndDisconnect()
+    {
+        $this->whatsProt->disconnect();
+    }
+}
